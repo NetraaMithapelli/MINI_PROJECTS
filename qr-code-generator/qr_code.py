e7c8a51ca10f32244a8be3f9d9bf0e61051da9bb
NetraaMithapelli <mithapellinetraa02@gmail.com>
2026-03-11 21:05:40 +0530

    Update qr_code.py

--- qr-code-generator/qr_code.py
+++ qr-code-generator/qr_code.py
@@ -1,1 +1,4 @@
+import qrcode as qr                                                        # imports library that generates QR code --> qr : alias (nickname)
+img = qr.make("https://www.youtube.com/channel/UC3XBkDeCVXCoCofFgfUZXGw")  #.make : creates QR code
+img.save("KaranAujla_youtube.png")                                         # saves the image of the qrcode
 
